79d6948550250775bf851e0833df9a3c2a8165e7
Miguel Eduardo <98851408+Kockiee@users.noreply.github.com>
2022-10-26 17:12:23 -0300

    Semi Game Addition
    Lol, interface for alterations is created to make this functional in the next version.

--- Kockie Optimizer Csharp/Games4.Designer.cs
+++ Kockie Optimizer Csharp/Games4.Designer.cs
@@ -28,21 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.chk2 = new Guna.UI2.WinForms.Guna2CustomRadioButton();
+            this.chk1 = new Guna.UI2.WinForms.Guna2CustomRadioButton();
+            this.btnOT1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox1.Image = global::Kockie_Optimizer_Csharp.Properties.Resources.league_of_legends_riot_games_logo_5;
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(54, 36);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(307, 131);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox1.TabIndex = 41;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // chk2
+            // 
+            this.chk2.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.chk2.CheckedState.BorderColor = System.Drawing.Color.Transparent;
+            this.chk2.CheckedState.BorderThickness = 0;
+            this.chk2.CheckedState.FillColor = System.Drawing.Color.RoyalBlue;
+            this.chk2.CheckedState.InnerColor = System.Drawing.Color.White;
+            this.chk2.CheckedState.InnerOffset = 10;
+            this.chk2.ForeColor = System.Drawing.Color.Transparent;
+            this.chk2.Location = new System.Drawing.Point(150, 233);
+            this.chk2.Name = "chk2";
+            this.chk2.Size = new System.Drawing.Size(25, 25);
+            this.chk2.TabIndex = 54;
+            this.chk2.Text = "guna2CustomRadioButton1";
+            this.chk2.UncheckedState.BorderColor = System.Drawing.Color.Transparent;
+            this.chk2.UncheckedState.BorderThickness = 2;
+            this.chk2.UncheckedState.FillColor = System.Drawing.Color.PaleTurquoise;
+            this.chk2.UncheckedState.InnerColor = System.Drawing.Color.PaleTurquoise;
+            this.chk2.CheckedChanged += new System.EventHandler(this.chk2_CheckedChanged);
+            // 
+            // chk1
+            // 
+            this.chk1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.chk1.CheckedState.BorderColor = System.Drawing.Color.Transparent;
+            this.chk1.CheckedState.BorderThickness = 0;
+            this.chk1.CheckedState.FillColor = System.Drawing.Color.RoyalBlue;
+            this.chk1.CheckedState.InnerColor = System.Drawing.Color.White;
+            this.chk1.CheckedState.InnerOffset = 10;
+            this.chk1.ForeColor = System.Drawing.Color.Transparent;
+            this.chk1.Location = new System.Drawing.Point(150, 183);
+            this.chk1.Name = "chk1";
+            this.chk1.Size = new System.Drawing.Size(25, 25);
+            this.chk1.TabIndex = 53;
+            this.chk1.Text = "guna2CustomRadioButton1";
+            this.chk1.UncheckedState.BorderColor = System.Drawing.Color.Transparent;
+            this.chk1.UncheckedState.BorderThickness = 2;
+            this.chk1.UncheckedState.FillColor = System.Drawing.Color.PaleTurquoise;
+            this.chk1.UncheckedState.InnerColor = System.Drawing.Color.PaleTurquoise;
+            this.chk1.CheckedChanged += new System.EventHandler(this.chk1_CheckedChanged);
+            // 
+            // btnOT1
+            // 
+            this.btnOT1.BorderRadius = 5;
+            this.btnOT1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnOT1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnOT1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnOT1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnOT1.FillColor = System.Drawing.Color.DodgerBlue;
+            this.btnOT1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnOT1.ForeColor = System.Drawing.Color.White;
+            this.btnOT1.Location = new System.Drawing.Point(110, 276);
+            this.btnOT1.Name = "btnOT1";
+            this.btnOT1.Size = new System.Drawing.Size(180, 45);
+            this.btnOT1.TabIndex = 52;
+            this.btnOT1.Text = "Mudar Config";
+            this.btnOT1.Click += new System.EventHandler(this.btnOT1_Click);
+            // 
+            // guna2HtmlLabel3
+            // 
+            this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.guna2HtmlLabel3.ForeColor = System.Drawing.Color.AliceBlue;
+            this.guna2HtmlLabel3.Location = new System.Drawing.Point(181, 235);
+            this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            this.guna2HtmlLabel3.Size = new System.Drawing.Size(78, 23);
+            this.guna2HtmlLabel3.TabIndex = 51;
+            this.guna2HtmlLabel3.Text = "PC FRACO";
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.AliceBlue;
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(181, 185);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(75, 23);
+            this.guna2HtmlLabel2.TabIndex = 50;
+            this.guna2HtmlLabel2.Text = "PC FORTE";
+            // 
+            // Games4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
             this.ClientSize = new System.Drawing.Size(755, 487);
+            this.Controls.Add(this.chk2);
+            this.Controls.Add(this.chk1);
+            this.Controls.Add(this.btnOT1);
+            this.Controls.Add(this.guna2HtmlLabel3);
+            this.Controls.Add(this.guna2HtmlLabel2);
+            this.Controls.Add(this.guna2PictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Form1";
+            this.Name = "Games4";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2CustomRadioButton chk2;
+        private Guna.UI2.WinForms.Guna2CustomRadioButton chk1;
+        private Guna.UI2.WinForms.Guna2Button btnOT1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private FolderBrowserDialog folderBrowserDialog1;
     }
 }
